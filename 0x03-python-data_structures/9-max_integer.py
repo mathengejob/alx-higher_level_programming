e8aa01974452051b8f22b75ee6ea643b42fe6f54
mathengejob <jobmathenge90@gmail.com>
2022-06-05 07:47:41 -0700

    Printing Max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 def max_integer(my_list=[]):
-    max_ = 0
     if len(my_list) < 0:
         return None
     else:
-        for number in my_list:
-            if number > max_:
-                max_ = number
-        return max_
+        my_list.sort()
+        return my_list[-1]
