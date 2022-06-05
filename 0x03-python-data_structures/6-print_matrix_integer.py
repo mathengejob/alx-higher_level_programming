dc4c677af7512677ae0bb08c3692f9fe5f76997b
mathengejob <jobmathenge90@gmail.com>
2022-06-05 06:37:30 -0700

    Printing matrix trial 1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for line in matrix:
-        for col in range(len(line)):
-            print(
-                    "{:d}".format(line[col]),
-                    end="" if col == len(line) - 1 else " ")
-            print("")
+        for col in line:
+            if col != line[-1]:
+                print("{:d}".format(col), end=" ")
+            else:
+                print("{:d}".format(col), end="")
+        print()
