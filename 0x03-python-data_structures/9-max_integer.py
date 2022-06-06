06dc3fab73890a250154400dfaa86104686b6f82
mathengejob <jobmathenge90@gmail.com>
2022-06-06 14:55:17 -0700

    Correction on Max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,8 @@
 def max_integer(my_list=[]):
-    if my_list and len(my_list) > 0:
-        return sorted(my_list)[-1]
-    else:
-        return None
+    if len(my_list) == 0:
+        return "None"
+    max_ = my_list[0]
+    for i in my_list:
+        if i > max_:
+            max_ = i
+    return max_
