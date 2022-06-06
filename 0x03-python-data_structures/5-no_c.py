d5e141bb9b9333b50f627a07ce2561c70aa2c286
mathengejob <jobmathenge90@gmail.com>
2022-06-06 14:34:12 -0700

    correction on deleting char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_string = ""
-    for i in my_string:
-        if i == "c" or i == "C":
-            pass
-        else:
-            new_string += i
-    return new_string
+    if my_string:
+        new_string = ""
+        for i in my_string:
+            if i is not "c" and i is not "C":
+                new_string += i
+        return new_string
