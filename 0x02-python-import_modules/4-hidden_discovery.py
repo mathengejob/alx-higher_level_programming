81c494553459cdec30a6fece81cabfb66d96fd53
mathengejob <jobmathenge90@gmail.com>
2022-06-08 00:47:56 -0700

    Reading names in a compiled file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
     import hidden_4
-    for i in dir(hidden_4):
+    fetch_dir = dir(hidden_4)
+    for i in fetch_dir:
         if i[:2] != "_":
             print(i)
