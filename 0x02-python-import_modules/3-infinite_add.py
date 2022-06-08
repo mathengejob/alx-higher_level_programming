d179fc42ce16a138a7ae52468fd1136a4ef806fc
mathengejob <jobmathenge90@gmail.com>
2022-06-07 22:37:12 -0700

    infinite add correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argv = sys.argv[1:]
-    if len(argv) > 1:
-        total = 0
-        for i in argv:
-            y = int(i, base=10)
-            total = total + y
-        print("{:d}".format(total))
+    from sys import argv
+    argv = argv[1:]
+    total = 0
+    for i in argv:
+        y = int(i)
+        total = total + y
+    print("{:d}".format(total))
