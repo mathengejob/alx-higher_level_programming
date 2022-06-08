aa6ca70a7312686fb62253f0a86b9b618b9e001d
mathengejob <jobmathenge90@gmail.com>
2022-06-08 02:31:24 -0700

    My calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
+    from calculator_1 import add, sub, mul, div
     argv = argv[1:]
 
     if len(argv) < 3 or len(argv) > 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        print("{:d}".format(1))
+        exit(1)
     elif argv[1] not in ["+", "-", "*", "/"]:
         print("Unknown operator. Available operators: +, -, * and /")
-        print("{:d}".format(1))
+        exit(1)
     else:
         a = int(argv[0])
         b = int(argv[2])
         if argv[1] == "+":
-            print("{:d}".format(a + b))
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
         if argv[1] == "-":
-            print("{:d}".format(a - b))
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
         if argv[1] == "*":
-            print("{:d}".format(a * b))
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
         if argv[1] == "/":
-            printi("{:d}".format(a / b))
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
